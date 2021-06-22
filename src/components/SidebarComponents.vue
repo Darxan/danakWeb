58b3e507d309fb7685e93f297a7110dc7bc07fbc
Abdurahmon2007 <truedevabduraxmon2007@gmail.com>
2021-06-22 13:24:06 +0500

    local change

---
+++ src/components/SidebarComponents.vue
@@ -0,0 +1,69 @@
+<template>
+  <div>  
+    <!-- sidebar boshlandi yangi o'zgarish -->
+    <div>
+      <div id="mySidenav" class="sidenav">
+        <!-- <a href=""><i class="fal fa-home-lg-alt"></i><span>Bosh sahifa</span></a>
+        <a href=""><i class="fal fa-home-lg-alt"></i><span>About</span></a>
+        <a href=""><i class="fal fa-home-lg-alt"></i><span>Mening naqd pulim</span></a>
+        <a href=""><i class="fal fa-home-lg-alt"></i><span>To'lov tarixi</span></a>
+        <a href=""><i class="fal fa-home-lg-alt"></i><span>Mening hamyonim</span></a>
+        <a href=""><i class="fal fa-home-lg-alt"></i><span>About</span></a>
+        <a href=""><i class="fal fa-home-lg-alt"></i><span>Sozlamalar</span></a> -->
+        <a :href="item.link" v-for="item in products">
+          <img :src="item.title" alt="">
+          <span> {{ item.title }} </span>
+        </a>
+      </div>
+  </div> 
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  data:() => {
+    return {
+      products: [
+                    {
+                        image: '@/assets/logo/home.png',
+                        title: 'iPhone XR',
+                        link: 'https://youtube.com',
+                    },
+                    {
+                        image: '@/assets/logo/home.png',
+                        title: 'iPhone XR',
+                        link: 'https://youtube.com',
+                    },
+                    {
+                        image: '@/assets/logo/home.png',
+                        title: 'iPhone XR',
+                        link: 'https://youtube.com',
+                    },
+                    {
+                        image: '@/assets/logo/home.png',
+                        title: 'iPhone XR',
+                        link: 'https://youtube.com',
+                    },
+                    {
+                        image: '@/assets/logo/home.png',
+                        title: 'iPhone XR',
+                        link: 'https://youtube.com',
+                    },
+                    {
+                      image: '@/assets/logo/home.png',
+                        title: 'iPhone XR',
+                        link: 'https://youtube.com',
+                    },
+                    
+                ],    
+    }
+  },
+    
+  };
+</script>
+
+<style scoped>
+  
+</style>
